afdb9c467b9d0bb00a0784ac3f80c86bb37396f8
thiozhaosheng <hello@travisthio.com>
2024-11-09 14:26:57 +0800

    fixed deployment issues

--- src/components/API Area/API.jsx
+++ src/components/API Area/API.jsx
@@ -1,26 +1,33 @@
-import React,{useState} from 'react'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TextField, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import './API.css'
+import React, { useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import {
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import "./API.css";
 
 const API = () => {
-
-
   return (
-    <div className='API'>
+    <div className="API">
       <h1>A better way to discover flexible flights</h1>
-      <p>Made for long-distance & budget travelers with flexible destinations and dates</p>
-      <div className='flight-search container'>
-        <div className='columns'>
+      <p>
+        Made for long-distance & budget travelers with flexible destinations and
+        dates
+      </p>
+      <div className="flight-search container">
+        <div className="columns">
           <div className="dropdowns">
             <select>
               <option>One way</option>
               <option>Round Trip</option>
             </select>
 
-            <select className='filters'>
+            <select className="filters">
               <option>Economy</option>
               <option>Business</option>
               <option>First Class</option>
@@ -28,21 +35,13 @@ const API = () => {
           </div>
 
           <div className="input-area">
-            <input className='input' placeholder='From'></input>
-            <input className='input' placeholder='To'></input>
+            <input className="input" placeholder="From"></input>
+            <input className="input" placeholder="To"></input>
           </div>
-
-          
-
-          
-
-
-
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default API
+export default API;
